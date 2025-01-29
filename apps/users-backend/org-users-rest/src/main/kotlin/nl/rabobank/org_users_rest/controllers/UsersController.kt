f9d206376418e3f39f73302da397d7438ff6c92f
Anton Cherepov <anton.cherepov@rabobank.nl>
2025-01-29 19:58:15 +0100

    add rest methods

--- apps/users-backend/org-users-rest/src/main/kotlin/nl/rabobank/org_users_rest/controllers/UsersController.kt
+++ apps/users-backend/org-users-rest/src/main/kotlin/nl/rabobank/org_users_rest/controllers/UsersController.kt
@@ -1,6 +1,11 @@
 package nl.rabobank.org_users_rest.controllers
 
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,5 +15,20 @@ class UsersController {
     @GetMapping("/", "")
     fun getUsers(): String {
         return "Hello"
+    }
+
+    @PostMapping("/", "")
+    fun addUser(@RequestBody userData: String): String {
+        return "Hello $userData"
+    }
+
+    @PutMapping("/{id}")
+    fun updateUser(@PathVariable id: String): String {
+        return "Hello $id"
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteUser(@PathVariable id: String): String {
+        return "Hello $id"
     }
 }
