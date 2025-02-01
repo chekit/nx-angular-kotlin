091660cfd562cf05878a84b7e4b66a55d5ce96e9
Anton Cherepov <anton.cherepov@rabobank.nl>
2025-02-01 12:04:26 +0100

    add basic service and repository

---
+++ apps/users-backend/org-users-rest/src/main/kotlin/nl/rabobank/org_users_rest/repository/UsersRepository.kt
@@ -0,0 +1,7 @@
+package nl.rabobank.org_users_rest.repository
+
+import org.springframework.stereotype.Repository
+
+@Repository
+class UsersRepository {
+}
---
+++ apps/users-backend/org-users-rest/src/main/kotlin/nl/rabobank/org_users_rest/service/UsersService.kt
@@ -0,0 +1,7 @@
+package nl.rabobank.org_users_rest.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class UsersService {
+}
