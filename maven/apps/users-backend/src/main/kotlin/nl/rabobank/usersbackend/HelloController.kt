832ddd9f780069baa8443ed7541261414f8f84c2
Anton Cherepov <chekitdnb@gmail.com>
2025-10-21 00:44:38 +0200

    adjust controller

--- maven/apps/users-backend/src/main/kotlin/nl/rabobank/usersbackend/HelloController.kt
+++ maven/apps/users-backend/src/main/kotlin/nl/rabobank/usersbackend/HelloController.kt
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.js.ExperimentalJsExport
 
 @RestController
 class HelloController {
--- maven/apps/users-backend/src/main/kotlin/nl/rabobank/usersbackend/models/UserData.kt
+++ maven/apps/users-backend/src/main/kotlin/nl/rabobank/usersbackend/models/UserData.kt
@@ -1,6 +1,3 @@
 package nl.rabobank.usersbackend.models
 
-import kotlin.js.ExperimentalJsExport
-
-@ExperimentalJsExport
 data class UserData(val name: String, val role: String) {}
