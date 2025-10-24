b563b8fe1c657f83562b742d40a83ad9dbc0e078
Anton Cherepov <chekitdnb@gmail.com>
2025-10-24 16:56:16 +0200

    rename maven to backend

--- backend/apps/users-backend/src/test/kotlin/nl/rabobank/usersbackend/HelloControllerTests.kt
+++ backend/apps/users-backend/src/test/kotlin/nl/rabobank/usersbackend/HelloControllerTests.kt
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
+import nl.rabobank.usersbackend.models.UserData
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloControllerTests(@Autowired val restTemplate: TestRestTemplate) {
@@ -19,11 +20,12 @@ class HelloControllerTests(@Autowired val restTemplate: TestRestTemplate) {
   }
 
   @Test
-  fun `Should return Hello World`() {
-    println(">> Should return Hello World")
+  fun `Should return payload`() {
+    println(">> Should return payload")
     val entity = restTemplate.getForEntity<String>("/")
     assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-    assertThat(entity.body).contains("Hello World")
+    assertThat(entity.body).contains("Test")
+    assertThat(entity.body).contains("Admin")
   }
 
 
