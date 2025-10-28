132418044b4b76ec1730af4ad258de4cc87f198e
Anton Cherepov <chekitdnb@gmail.com>
2025-10-28 22:30:00 +0100

    reuse data from backend, update collections

---
+++ backend/apps/users-backend/src/main/kotlin/nl/rabobank/usersbackend/entity/Role.ts
@@ -0,0 +1,5 @@
+// https://www.codeconvert.ai/kotlin-to-typescript-converter?id=d7662bd0-6512-40f7-845f-508037b413b4
+export interface Role {
+  id?: number;
+  name: string;
+}
---
+++ backend/apps/users-backend/src/main/kotlin/nl/rabobank/usersbackend/entity/User.ts
@@ -0,0 +1,9 @@
+import { Role } from './Role';
+
+// https://www.codeconvert.ai/kotlin-to-typescript-converter?id=13f491e6-50ce-4820-a33f-26de3eea9451
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  role: Role;
+}
